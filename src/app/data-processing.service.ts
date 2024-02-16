202602d37ac2853df8e6277e2af779c81268efac
Alex Tremblay <atremblay@ironflytechnologies>
2024-02-16 16:11:09 +1100

    stuf

---
+++ src/app/data-processing.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataProcessingService {
+
+  constructor() { }
+
+
+  async generateData(){}
+
+  async encrypt(){}
+
+  async lookup(){}
+
+  async castToObject(){}
+
+  async 
+}
